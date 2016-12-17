831910c47bdf00cf60da625cc5a3c0521ed08fb6
Kalab J Oster <Kalab_J_Oster@localhost>
2016-12-17 13:46:32 -0800

    Read and edit. Write some process to define the chapters of the story. I am using yaml for it seems to be the most popular format right now given its ease for a human to read. It is a good configuration format. I am also using this in other code that I am not controlling the dev path, and I have come to alike it. This code works. Needs work like testing. Which I may try to get to before moving on to the next process in the pipeline.

--- examples/tagonists/input/deepdive_input_formater_tsv.py
+++ examples/tagonists/input/deepdive_input_formater_tsv.py
@@ -1,7 +1,7 @@
 #!usr/bin/env python
 # 
 #######
-# Purpose = Read and format book chapter data in on tsv file for this deepdive app.
+# Purpose = Read and format story chapter data in on tsv file for this deepdive app.
 #######
 # This command line tool reads from a configuration 
 # files a list of file that are chapters of a story.
@@ -10,10 +10,13 @@
 # into a tsv list file compatiable with deepdive and 
 # a version of a pipeline.
 #######
+import yaml
 
+with open('story.yaml', 'r') as y:
+    story = yaml.load(y)
 
-
-
+for key, value in story['chapter'].iteritems():
+    print(key, value)
 
 
 
