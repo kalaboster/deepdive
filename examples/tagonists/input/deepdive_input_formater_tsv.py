3ec1125db1def8873398e16f42c8e453dfe23551
Kalab J Oster <Kalab_J_Oster@localhost>
2016-12-17 12:53:29 -0800

    Write the tools purpose in a heading as I begin to write the idea.

--- examples/tagonists/input/deepdive_input_formater_tsv.py
+++ examples/tagonists/input/deepdive_input_formater_tsv.py
@@ -1,3 +1,35 @@
+#!usr/bin/env python
+# 
+#######
+# Purpose = Read and format book chapter data in on tsv file for this deepdive app.
+#######
+# This command line tool reads from a configuration 
+# files a list of file that are chapters of a story.
+#
+# Once the files are read the the script publishes them 
+# into a tsv list file compatiable with deepdive and 
+# a version of a pipeline.
+#######
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
