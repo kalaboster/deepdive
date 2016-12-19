a257aaf43c9a8f1f14e75d8a5d999bdb7a26babf
Kalab J Oster <Kalab_J_Oster@localhost>
2016-12-19 06:01:21 -0800

    Edit the yaml to have it point to the short story. ...Write the logic in script to read in a chapter story or a short story and begin to write the process to get this into a tsv list needed. Publish because shit sort of works and during this time and life I need to move from this interface.

--- examples/tagonists/input/deepdive_input_formater_tsv.py
+++ examples/tagonists/input/deepdive_input_formater_tsv.py
@@ -1,13 +1,13 @@
 #!usr/bin/env python
-# 
+#
 #######
 # Purpose = Read and format story chapter data in on tsv file for this deepdive app.
 #######
-# This command line tool reads from a configuration 
+# This command line tool reads from a configuration
 # files a list of file that are chapters of a story.
 #
-# Once the files are read the the script publishes them 
-# into a tsv list file compatiable with deepdive and 
+# Once the files are read the the script publishes them
+# into a tsv list file compatiable with deepdive and
 # a version of a pipeline.
 #######
 import yaml
@@ -15,24 +15,26 @@ import yaml
 with open('story.yaml', 'r') as y:
     story = yaml.load(y)
 
-for key, value in story['chapter'].iteritems():
-    print(key, value)
+# if there are chapters then check and create tsv
+if 'chapter' in story:
+    for key, value in story['chapter'].iteritems():
+        print(key, value)
 
 
+if 'short' in story:
+
+    print("This is s short story:")
+    print(story['short'])
+
+    with open(story['short']) as f:
+
+        storylines = f.readlines()
+        for line in storylines:
+
+            if not line.startswith("#"):
+                print line.strip()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+with open("story.tsv", "w") as tsv:
+    print("What the fuck to I print?")
+    print "\t".join(tsv)
