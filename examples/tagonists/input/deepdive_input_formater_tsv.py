f4807a6d9a1deec7033ea2f8cf81eb4f6f809218
Kalab J Oster <Kalab_J_Oster@localhost>
2016-12-21 05:46:28 -0800

    Edit story to remove tabs because I am tired of looking at the tabs. ...Edit and write deepdive script. The script appears to work enough to make me believe I should commit the file. I need to rework and probably change the yaml and how the story is defined. I likely only need a list of possible chapters or chapters or files. I am thinking. Right now. This creates something to make me believe I can code forth and begin to make my own example. Publish because it feels to work enough. It may. I have not tests yet or have not test, so it's all feelings and bullshit right now.

--- examples/tagonists/input/deepdive_input_formater_tsv.py
+++ examples/tagonists/input/deepdive_input_formater_tsv.py
@@ -13,37 +13,36 @@
 import yaml
 import uuid
 
-
-story = ""
-
-print uuid.uuid4()
-
-
 with open('story.yaml', 'r') as y:
     story = yaml.load(y)
 
 # if there are chapters then check and create tsv
+# This does nothing right now but print.
+# I am leaving this as I may not have two options.
+# It seems to me only a list is needed.
+# I don't want to think of the right words now
+# That don't include chapter.
 if 'chapter' in story:
+
     for key, value in story['chapter'].iteritems():
+
         print(key, value)
 
-
+# Read in the short story and make into one tsv.
 if 'short' in story:
 
-    print("This is s short story:")
-    print(story['short'])
+    short = ""
 
     with open(story['short']) as f:
 
-        storylines = f.readlines()
-        for line in storylines:
+        for line in f:
 
             if not line.startswith("#"):
-                print "A Line:" + line.strip()
-                story = story + "\n\n" + str(line.strip())
 
-print story
+                short = short + line.strip() + "\\n"
 
-with open("story.tsv", "w") as tsv:
-    print("What the fuck to I print?")
-    print "\t" .join(story)
+    with open("story.tsv", "w") as tsv:
+
+        print("Print story.tsv files of the story: " + story['short'])
+
+        tsv.write(str(uuid.uuid4()) + "\t" + short)
